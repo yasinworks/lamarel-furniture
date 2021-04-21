2bac732d199a7dc6e8b5ff3d3787961b963d2a58
scripto <scripto@MacBook-Air-scripto.local>
2021-04-21 19:31:00 +0300

    mobile | unit page done

--- src/js/rangeSlider.js
+++ src/js/rangeSlider.js
@@ -1,11 +1,13 @@
 let slider = document.getElementById('range-slider');
 
-noUiSlider.create(slider, {
-    start: [10000, 25000],
-    connect: true,
-    step: 1,
-    range: {
-        'min': 2500,
-        'max': 35000
-    }
-});
+if (slider) {
+    noUiSlider.create(slider, {
+        start: [10000, 25000],
+        connect: true,
+        step: 1,
+        range: {
+            'min': 2500,
+            'max': 35000
+        }
+    });
+}
